bcaaf76b619f13b95b1d0273adbaa73eb0a8f667
davidmurilloramos <dmurillo11.madrid@gmail.com>
2015-05-19 17:30:15 +0200

    Estadisticas adaptadas a heroku

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var statisticController=require("../controllers/statistic_controller");
+
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
@@ -28,6 +30,10 @@ router.post('/quizes/create',              sessionController.loginRequired, quiz
 router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
+
+// Definición de ruta de estadísticas
+router.get('/quizes/statistics', statisticController.index);
+
 // Definición de ruta de autor
 router.get("/author", function(req, res) {
 	res.render("author", {errors: []});
